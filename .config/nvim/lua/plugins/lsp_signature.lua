bbf7d2321fb6d49bd8fc12a18bb3b410982003a2
Kristian Ebdrup <kristian.ebdrup@alexandra.dk>
2025-09-12 22:57:16 +0200

    Update lualine, treesitter and lsp config

--- .config/nvim/lua/plugins/lsp_signature.lua
+++ .config/nvim/lua/plugins/lsp_signature.lua
@@ -1,10 +1,12 @@
 return {
-  "ray-x/lsp_signature.nvim",
-  event = "InsertEnter",
-  opts = {
-    bind = true,
-    handler_opts = {
-      border = "rounded"
-    }
-  },
+	"ray-x/lsp_signature.nvim",
+	event = "InsertEnter",
+	opts = {
+		bind = true,
+		doc_lines = 0,
+		hint_enable = false,
+		handler_opts = {
+			border = "rounded",
+		},
+	},
 }
--- .config/nvim/lua/plugins/nvim_treesitter.lua
+++ .config/nvim/lua/plugins/nvim_treesitter.lua
@@ -1,16 +1,27 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter")
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", 'typescript', 'python', "tsx", "go", python },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      vim.treesitter.language.register('tsx', 'typescriptreact')
-      vim.api.nvim_set_hl(0, "tsxCloseString", { link = "htmlTag" }) -- Fix green closing tags
-      vim.cmd "TSEnable highlight"
-    end
- }
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+				"html",
+				"typescript",
+				"python",
+				"tsx",
+				"go",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+		vim.treesitter.language.register("tsx", "typescriptreact")
+		vim.api.nvim_set_hl(0, "tsxCloseString", { link = "htmlTag" }) -- Fix green closing tags
+		vim.cmd("TSEnable highlight")
+	end,
+}
