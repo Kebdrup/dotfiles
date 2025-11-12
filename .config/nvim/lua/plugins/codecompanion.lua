a23ff5a9461673bd8b42a51bf073cb93e954a1df
Kristian Ebdrup <kristian.ebdrup@alexandra.dk>
2025-11-12 09:22:07 +0100

    Add copilot

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,50 @@
+return {
+	"olimorris/codecompanion.nvim",
+	opts = {
+		strategies = {
+			chat = {
+				name = "copilot",
+				model = "claude-sonnet-4.5",
+			},
+			inline = {
+				name = "copilot",
+			},
+			agent = {
+				name = "copilot",
+			},
+		},
+		-- NOTE: The log_level is in `opts.opts`
+		opts = {
+			log_level = "DEBUG",
+		},
+	},
+	keys = {
+		{
+			"<leader>ac", -- Open CodeCompanion chat
+			"<cmd>CodeCompanionChat<CR>",
+			desc = "Open CodeCompanion Chat",
+		},
+		{
+			"<leader>ar", -- Run code review
+			"<cmd>CodeCompanionReview<CR>",
+			desc = "CodeCompanion Review Selected Code",
+			mode = { "v" },
+		},
+		{
+			"<leader>at", -- Generate tests
+			"<cmd>CodeCompanionTest<CR>",
+			desc = "CodeCompanion Generate Tests",
+			mode = { "v" },
+		},
+		{
+			"<leader>af", -- Propose fixes for selected code
+			"<cmd>CodeCompanionFix<CR>",
+			desc = "CodeCompanion Propose Fixes",
+			mode = { "v" },
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"github/copilot.vim",
+	},
+}
