65f2d53627d15deb6d0b73ced6d7f5e92efeed45
Kristian Ebdrup <kristian.ebdrup@alexandra.dk>
2026-05-30 14:01:11 +0200

    Add curl to apt and refactor config

--- config_install.sh
+++ config_install.sh
@@ -5,10 +5,10 @@ current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 setup_container() {
   # Install utilities
-  apt update && apt install -y git zsh build-essential
+  apt update && apt install -y curl git zsh build-essential
 
   # Install oh-my-zsh
-  RUNZSH=no CHSH=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  RUNZSH=no CHSH=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
   # Install mise-en-place
   curl https://mise.run | MISE_INSTALL_PATH=/usr/local/bin/mise sh
@@ -16,23 +16,21 @@ setup_container() {
   # Add home configs
   cp -v -a -r $current_path/home/. $HOME/
 
-  mise --cd ~/.config/mise/ trust 
-  mise --cd ~/.config/mise/ install 
+  mise install --cd ~/.config/mise/
 
   # Add bash configs
   $current_path/bash/extend_bashrc.sh
-  source ~/.bashrc
 }
 
 replace_configs() {
-  mkdir -p $config_dir
-  echo "Using config directory: $config_dir"
-  cp -v -R $current_path/.config/ $config_dir 
+  cp -v -R $current_path/.config/ $HOME/.config
+  mise trust "$current_path/.config/mise/mise.toml"
+  mise trust ~/.config/mise/mise.toml
 }
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    --config|-c)  config_dir="$2"; replace_configs; shift 2 ;;
+    --config|-c)  replace_configs; shift ;;
     --init|-i)    setup_container; shift ;;
     --help|-h)    echo "Usage: $0 [--init|-i] [--config|-c path]"; exit 0 ;;
     -*)           echo "Error: Unknown option $1" >&2; exit 1 ;;
